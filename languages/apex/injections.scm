20263e6a1d555c3346e8b2fbed8d5dda10fadea5
Justin Benge <jbenge1@arizona.edu>
2024-08-26 14:36:36 +0200

    Added soql support

--- languages/apex/injections.scm
+++ languages/apex/injections.scm
@@ -1,5 +1,11 @@
+; Comments
 ([
   (line_comment)
   (block_comment)
 ] @injection.content
   (#set! injection.language "comment"))
+
+((query_expression
+  (soql_query) @injection.content)
+ (#set! injection.language "soql")
+ (#set! injection.include-children))
